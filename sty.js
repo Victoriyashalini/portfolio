30b00632f3bcc699f1856c43a44b88d25958e168
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-02-09 11:01:15 +0530

    Delete sty.js
    Signed-off-by: Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>

--- sty.js
+++
@@ -1,9 +0,0 @@
-const sentenceContainer = document.getElementById('sentence-container');
-const sentence = 'Hello, there';
-
-sentence.split('').forEach((char, index) => {
-  const span = document.createElement('span');
-  span.textContent = char;
-  span.style.animationDelay = `${index * 0.1}s`; // Adjust the delay for a staggered effect
-  sentenceContainer.appendChild(span);
-});
