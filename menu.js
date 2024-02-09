31d01f4f321175f0a427f9b21feeab7647682d89
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-02-09 11:10:49 +0530

    Delete menu.js
    Signed-off-by: Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>

--- menu.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.navbar-toggler').addEventListener('click', function () {
-	// Toggle the 'fa-bars' and 'fa-times' classes for Font Awesome icons
-	this.querySelector('i').classList.toggle('fa-bars');
-	this.querySelector('i').classList.toggle('fa-times');
-});
